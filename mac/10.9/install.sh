9e726d1fa0a274bfd1b3d7a1ed91a609e1abdb67
Shelby Doolittle <shelby@shelbyd.com>
2014-10-29 11:32:14 -0700

    add install script for mac 10.9

---
+++ mac/10.9/install.sh
@@ -0,0 +1,4 @@
+cp -r Tarmak\ 1.bundle/ /Library/Keyboard\ Layouts/Tarmak\ 1.bundle/
+cp -r Tarmak\ 2.bundle/ /Library/Keyboard\ Layouts/Tarmak\ 2.bundle/
+cp -r Tarmak\ 3.bundle/ /Library/Keyboard\ Layouts/Tarmak\ 3.bundle/
+cp -r Tarmak\ 4.bundle/ /Library/Keyboard\ Layouts/Tarmak\ 4.bundle/
